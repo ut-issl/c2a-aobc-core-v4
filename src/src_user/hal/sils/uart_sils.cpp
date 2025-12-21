c0888b01dd52393207b30afcd950f2799f8e5273
fukudakazuya <namiharrypotter@yahoo.co.jp>
2025-12-21 22:02:08 +0900

    Modify hal

--- src/src_user/hal/sils/uart_sils.cpp
+++ src/src_user/hal/sils/uart_sils.cpp
@@ -1,13 +1,18 @@
 #pragma section REPRO
-#include <src_core/hal/uart.h>
-#include "../../settings/port_config.h"
 
-#ifdef USE_SCI_COM_WINGS
-#include "uart_sils_sci_if.hpp"
+#include "uart_sils.h"
+
+#include <src_core/IfWrapper/uart.h>
+
+
+#ifdef USE_UART_COM
+#include "com_port.h"
+#include "../../Settings/port_config.h"
+#include "../../Settings/sils_port_config.h"
+
+static ComPort uart_sils_com_port(COM_PORT_SILS);
 #endif
 
-int OBC_C2A_SendFromObc(int port_id, unsigned char* buffer, int offset, int count);
-int OBC_C2A_ReceivedByObc(int port_id, unsigned char* buffer, int offset, int count);
 
 int UART_init(void* my_uart_v)
 {
@@ -18,13 +23,15 @@ int UART_rx(void* my_uart_v, void* data_v, int buffer_size)
 {
   UART_Config* my_uart = (UART_Config*)my_uart_v;
 
-  if (my_uart->ch != PORT_CH_RS422_MOBC_EXT)
+#ifdef USE_UART_COM
+  if (my_uart->ch == PORT_CH_UART_MOBC)
+  {
+    return uart_sils_com_port.receive((unsigned char*)data_v, 0, buffer_size);
+  }
+  else
   {
     return OBC_C2A_ReceivedByObc(my_uart->ch, (unsigned char*)data_v, 0, buffer_size);
   }
-
-#ifdef USE_SCI_COM_WINGS
-  return SILS_SCI_UART_IF_RX((unsigned char*)data_v, buffer_size);
 #else
   return OBC_C2A_ReceivedByObc(my_uart->ch, (unsigned char*)data_v, 0, buffer_size);
 #endif
@@ -34,24 +41,21 @@ int UART_tx(void* my_uart_v, void* data_v, int data_size)
 {
   UART_Config* my_uart = (UART_Config*)my_uart_v;
 
-  if (my_uart->ch != PORT_CH_RS422_MOBC_EXT)
+#ifdef USE_UART_COM
+  if (my_uart->ch == PORT_CH_UART_MOBC)
   {
-    if (OBC_C2A_SendFromObc(my_uart->ch, (unsigned char*)data_v, 0, data_size) < 0)
-    {
-      return -1;
-    }
-    else
-    {
-      return 0;
-    }
+    uart_sils_com_port.send((unsigned char*)data_v, 0, data_size);
   }
-#ifdef USE_SCI_COM_WINGS
-  SILS_SCI_UART_IF_TX((unsigned char*)data_v, data_size);
+  else
+  {
+    int result = OBC_C2A_SendFromObc(my_uart->ch, (unsigned char*)data_v, 0, data_size);
+
+    if (result < 0) return -1;
+  }
 #else
-  if (OBC_C2A_SendFromObc(my_uart->ch, (unsigned char*)data_v, 0, data_size) < 0)
-  {
-    return -1;
-  }
+  int result = OBC_C2A_SendFromObc(my_uart->ch, (unsigned char*)data_v, 0, data_size);
+
+  if (result < 0) return -1;
 #endif
 
   return 0;
