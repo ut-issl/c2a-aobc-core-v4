88f401885a1d69e27d7f81f39f769888f3a8eedb
fukudakazuya <namiharrypotter@yahoo.co.jp>
2025-12-21 18:08:01 +0900

    Modify applications

--- src/src_user/applications/component_service/csrv_mobc.h
+++ src/src_user/applications/component_service/csrv_mobc.h
@@ -1,29 +1,28 @@
 /**
- * @file
- * @brief MOBC の Component Service． MOBC における GS との CSRV に相当する
- */
-#ifndef CSRV_MOBC_H_
-#define CSRV_MOBC_H_
+* @file
+* @brief MOBC の DI. MOBC における GS との DI に相当する
+*/
+#ifndef DI_MOBC_H_
+#define DI_MOBC_H_
 
-#include "../../component_driver/etc/mobc.h"
-#include <src_core/system/application_manager/app_info.h>
-#include <src_core/tlm_cmd/common_packet/common_cmd_packet.h>
+#include <src_user/Drivers/Etc/mobc.h>
+#include <src_core/System/ApplicationManager/app_info.h>
+#include <src_core/TlmCmd/common_cmd_packet.h>
 
 extern const MOBC_Driver* const mobc_driver;           //!< MOBC driver
 
 /**
- * @brief  MOBC update アプリ生成関数
+ * @brief  MOBC updateアプリ生成関数
  * @param  void
  * @return AppInfo
  */
-AppInfo CSRV_MOBC_update(void);
+AppInfo DI_MOBC_update(void);
 
 /**
- * @brief  MOBC RT_TLM (Realtime Telemetry) PH (packet handler) アプリ生成関数
+ * @brief  MOBC RT TLM (Realtime telemetry) PH (packet handler) アプリ生成関数
  * @param  void
  * @return AppInfo
- * @note   ひとまずテレメは RT_TLM のみ実装する
  */
-AppInfo CSRV_MOBC_rt_tlm_packet_handler(void);
+AppInfo DI_MOBC_rt_tlm_packet_handler(void);
 
 #endif
