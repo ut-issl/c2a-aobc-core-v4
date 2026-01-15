4d197f79387eef7063ff5d30040a5175a9193ac4
fukudakazuya <namiharrypotter@yahoo.co.jp>
2026-01-15 22:57:12 +0900

    Modify src/src_user/tlm_cmd

--- src/src_user/tlm_cmd/command_definitions.c
+++ src/src_user/tlm_cmd/command_definitions.c
@@ -12,11 +12,6 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
 {
   cmd_table[Cmd_CODE_NOP].cmd_func = Cmd_NOP;
   cmd_table[Cmd_CODE_TMGR_SET_TIME].cmd_func = Cmd_TMGR_SET_TIME;
-  cmd_table[Cmd_CODE_TMGR_UPDATE_UNIXTIME].cmd_func = Cmd_TMGR_UPDATE_UNIXTIME;
-  cmd_table[Cmd_CODE_TMGR_SET_UTL_UNIXTIME_EPOCH].cmd_func = Cmd_TMGR_SET_UTL_UNIXTIME_EPOCH;
-  cmd_table[Cmd_CODE_TMGR_SET_CYCLE_CORRECTION].cmd_func = Cmd_TMGR_SET_CYCLE_CORRECTION;
-  cmd_table[Cmd_CODE_TMGR_RESET_CYCLE_CORRECTION].cmd_func = Cmd_TMGR_RESET_CYCLE_CORRECTION;
-  cmd_table[Cmd_CODE_TMGR_CLEAR_UNIXTIME_INFO].cmd_func = Cmd_TMGR_CLEAR_UNIXTIME_INFO;
   cmd_table[Cmd_CODE_AM_REGISTER_APP].cmd_func = Cmd_AM_REGISTER_APP;
   cmd_table[Cmd_CODE_AM_INITIALIZE_APP].cmd_func = Cmd_AM_INITIALIZE_APP;
   cmd_table[Cmd_CODE_AM_EXECUTE_APP].cmd_func = Cmd_AM_EXECUTE_APP;
@@ -26,7 +21,6 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_MM_SET_TRANSITION_TABLE].cmd_func = Cmd_MM_SET_TRANSITION_TABLE;
   cmd_table[Cmd_CODE_MM_START_TRANSITION].cmd_func = Cmd_MM_START_TRANSITION;
   cmd_table[Cmd_CODE_MM_FINISH_TRANSITION].cmd_func = Cmd_MM_FINISH_TRANSITION;
-  cmd_table[Cmd_CODE_MM_UPDATE_TRANSITION_TABLE_FOR_TLM].cmd_func = Cmd_MM_UPDATE_TRANSITION_TABLE_FOR_TLM;
   cmd_table[Cmd_CODE_TDSP_SET_TASK_LIST].cmd_func = Cmd_TDSP_SET_TASK_LIST;
   cmd_table[Cmd_CODE_TLCD_CLEAR_ALL_TIMELINE].cmd_func = Cmd_TLCD_CLEAR_ALL_TIMELINE;
   cmd_table[Cmd_CODE_TLCD_CLEAR_TIMELINE_AT].cmd_func = Cmd_TLCD_CLEAR_TIMELINE_AT;
@@ -36,16 +30,11 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_TLCD_SET_LOUT_FLAG].cmd_func = Cmd_TLCD_SET_LOUT_FLAG;
   cmd_table[Cmd_CODE_TLCD_SET_ID_FOR_TLM].cmd_func = Cmd_TLCD_SET_ID_FOR_TLM;
   cmd_table[Cmd_CODE_TLCD_SET_PAGE_FOR_TLM].cmd_func = Cmd_TLCD_SET_PAGE_FOR_TLM;
-  cmd_table[Cmd_CODE_CDIS_MGR_SET_IDX_FOR_TLM].cmd_func = Cmd_CDIS_MGR_SET_IDX_FOR_TLM;
   cmd_table[Cmd_CODE_GENERATE_TLM].cmd_func = Cmd_GENERATE_TLM;
   cmd_table[Cmd_CODE_TG_GENERATE_TLM].cmd_func = Cmd_TG_GENERATE_TLM;
   cmd_table[Cmd_CODE_TG_GENERATE_HP_TLM].cmd_func = Cmd_TG_GENERATE_HP_TLM;
   cmd_table[Cmd_CODE_TG_GENERATE_RT_TLM].cmd_func = Cmd_TG_GENERATE_RT_TLM;
   cmd_table[Cmd_CODE_TG_GENERATE_ST_TLM].cmd_func = Cmd_TG_GENERATE_ST_TLM;
-  cmd_table[Cmd_CODE_TG_FORWARD_TLM].cmd_func = Cmd_TG_FORWARD_TLM;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_HP_TLM].cmd_func = Cmd_TG_FORWARD_AS_HP_TLM;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_RT_TLM].cmd_func = Cmd_TG_FORWARD_AS_RT_TLM;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_ST_TLM].cmd_func = Cmd_TG_FORWARD_AS_ST_TLM;
   cmd_table[Cmd_CODE_BCT_CLEAR_BLOCK].cmd_func = Cmd_BCT_CLEAR_BLOCK;
   cmd_table[Cmd_CODE_BCT_SET_BLOCK_POSITION].cmd_func = Cmd_BCT_SET_BLOCK_POSITION;
   cmd_table[Cmd_CODE_BCT_COPY_BCT].cmd_func = Cmd_BCT_COPY_BCT;
@@ -60,9 +49,6 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_BCE_RESET_ROTATOR_INFO].cmd_func = Cmd_BCE_RESET_ROTATOR_INFO;
   cmd_table[Cmd_CODE_BCE_RESET_COMBINER_INFO].cmd_func = Cmd_BCE_RESET_COMBINER_INFO;
   cmd_table[Cmd_CODE_BCE_SET_ROTATE_INTERVAL].cmd_func = Cmd_BCE_SET_ROTATE_INTERVAL;
-  cmd_table[Cmd_CODE_GSCD_CLEAR_ERR_LOG].cmd_func = Cmd_GSCD_CLEAR_ERR_LOG;
-  cmd_table[Cmd_CODE_RTCD_CLEAR_ALL_REALTIME].cmd_func = Cmd_RTCD_CLEAR_ALL_REALTIME;
-  cmd_table[Cmd_CODE_RTCD_CLEAR_ERR_LOG].cmd_func = Cmd_RTCD_CLEAR_ERR_LOG;
   cmd_table[Cmd_CODE_EL_INIT].cmd_func = Cmd_EL_INIT;
   cmd_table[Cmd_CODE_EL_CLEAR_LOG_ALL].cmd_func = Cmd_EL_CLEAR_LOG_ALL;
   cmd_table[Cmd_CODE_EL_CLEAR_LOG_BY_ERR_LEVEL].cmd_func = Cmd_EL_CLEAR_LOG_BY_ERR_LEVEL;
@@ -75,12 +61,8 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_EL_INIT_LOGGING_SETTINGS].cmd_func = Cmd_EL_INIT_LOGGING_SETTINGS;
   cmd_table[Cmd_CODE_EL_ENABLE_LOGGING].cmd_func = Cmd_EL_ENABLE_LOGGING;
   cmd_table[Cmd_CODE_EL_DISABLE_LOGGING].cmd_func = Cmd_EL_DISABLE_LOGGING;
-  cmd_table[Cmd_CODE_EL_ENABLE_LOGGING_ALL].cmd_func = Cmd_EL_ENABLE_LOGGING_ALL;
-  cmd_table[Cmd_CODE_EL_DISABLE_LOGGING_ALL].cmd_func = Cmd_EL_DISABLE_LOGGING_ALL;
   cmd_table[Cmd_CODE_EL_ENABLE_TLOG_OVERWRITE].cmd_func = Cmd_EL_ENABLE_TLOG_OVERWRITE;
   cmd_table[Cmd_CODE_EL_DISABLE_TLOG_OVERWRITE].cmd_func = Cmd_EL_DISABLE_TLOG_OVERWRITE;
-  cmd_table[Cmd_CODE_EL_ENABLE_TLOG_OVERWRITE_ALL].cmd_func = Cmd_EL_ENABLE_TLOG_OVERWRITE_ALL;
-  cmd_table[Cmd_CODE_EL_DISABLE_TLOG_OVERWRITE_ALL].cmd_func = Cmd_EL_DISABLE_TLOG_OVERWRITE_ALL;
   cmd_table[Cmd_CODE_EH_INIT].cmd_func = Cmd_EH_INIT;
   cmd_table[Cmd_CODE_EH_CLEAR_ALL_RULE].cmd_func = Cmd_EH_CLEAR_ALL_RULE;
   cmd_table[Cmd_CODE_EH_LOAD_DEFAULT_RULE].cmd_func = Cmd_EH_LOAD_DEFAULT_RULE;
@@ -121,18 +103,10 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_CA_REGISTER_CMD].cmd_func = Cmd_CA_REGISTER_CMD;
   cmd_table[Cmd_CODE_TF_SET_PAGE_FOR_TLM].cmd_func = Cmd_TF_SET_PAGE_FOR_TLM;
   cmd_table[Cmd_CODE_CA_SET_PAGE_FOR_TLM].cmd_func = Cmd_CA_SET_PAGE_FOR_TLM;
-  cmd_table[Cmd_CODE_WDT_INIT].cmd_func = Cmd_WDT_INIT;
-  cmd_table[Cmd_CODE_WDT_ENABLE].cmd_func = Cmd_WDT_ENABLE;
-  cmd_table[Cmd_CODE_WDT_DISABLE].cmd_func = Cmd_WDT_DISABLE;
-  cmd_table[Cmd_CODE_WDT_STOP_CLEAR].cmd_func = Cmd_WDT_STOP_CLEAR;
-  cmd_table[Cmd_CODE_WDT_START_CLEAR].cmd_func = Cmd_WDT_START_CLEAR;
+  cmd_table[Cmd_CODE_GSCD_CLEAR_ERR_LOG].cmd_func = Cmd_GSCD_CLEAR_ERR_LOG;
+  cmd_table[Cmd_CODE_RTCD_CLEAR_ERR_LOG].cmd_func = Cmd_RTCD_CLEAR_ERR_LOG;
 
   cmd_table[Cmd_CODE_TMGR_SET_TIME].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_4BYTE;
-  cmd_table[Cmd_CODE_TMGR_UPDATE_UNIXTIME].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_8BYTE;
-  cmd_table[Cmd_CODE_TMGR_UPDATE_UNIXTIME].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_4BYTE;
-  cmd_table[Cmd_CODE_TMGR_UPDATE_UNIXTIME].param_size_infos[1].packed_info.bit.first = CA_PARAM_SIZE_TYPE_4BYTE;
-  cmd_table[Cmd_CODE_TMGR_SET_UTL_UNIXTIME_EPOCH].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_8BYTE;
-  cmd_table[Cmd_CODE_TMGR_SET_CYCLE_CORRECTION].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_8BYTE;
   cmd_table[Cmd_CODE_AM_REGISTER_APP].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_4BYTE;
   cmd_table[Cmd_CODE_AM_REGISTER_APP].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_4BYTE;
   cmd_table[Cmd_CODE_AM_REGISTER_APP].param_size_infos[1].packed_info.bit.first = CA_PARAM_SIZE_TYPE_4BYTE;
@@ -158,7 +132,6 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_TLCD_SET_LOUT_FLAG].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_TLCD_SET_ID_FOR_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_TLCD_SET_PAGE_FOR_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_CDIS_MGR_SET_IDX_FOR_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_GENERATE_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_GENERATE_TLM].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_GENERATE_TLM].param_size_infos[1].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
@@ -170,18 +143,6 @@ void CA_load_cmd_table(CA_CmdInfo cmd_table[CA_MAX_CMDS])
   cmd_table[Cmd_CODE_TG_GENERATE_RT_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_TG_GENERATE_ST_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_TG_GENERATE_ST_TLM].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_2BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_TLM].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_TLM].param_size_infos[1].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_TLM].param_size_infos[1].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_TLM].param_size_infos[2].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_HP_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_2BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_HP_TLM].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_RT_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_2BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_RT_TLM].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_ST_TLM].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_2BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_ST_TLM].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
-  cmd_table[Cmd_CODE_TG_FORWARD_AS_ST_TLM].param_size_infos[1].packed_info.bit.first = CA_PARAM_SIZE_TYPE_1BYTE;
   cmd_table[Cmd_CODE_BCT_CLEAR_BLOCK].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_2BYTE;
   cmd_table[Cmd_CODE_BCT_SET_BLOCK_POSITION].param_size_infos[0].packed_info.bit.first = CA_PARAM_SIZE_TYPE_2BYTE;
   cmd_table[Cmd_CODE_BCT_SET_BLOCK_POSITION].param_size_infos[0].packed_info.bit.second = CA_PARAM_SIZE_TYPE_1BYTE;
--- src/src_user/tlm_cmd/normal_block_command_definition/nbc_cdh.c
+++ src/src_user/tlm_cmd/normal_block_command_definition/nbc_cdh.c
@@ -10,8 +10,6 @@
 
 void BCL_load_hk_cyclic_tlm(void)
 {
-  BCL_tool_prepare_param_uint8(Tlm_CODE_AOBC_HK);
-  BCL_tool_register_cmd(1, Cmd_CODE_TG_GENERATE_RT_TLM);
   BCL_tool_register_deploy(10, BC_HK_CYCLIC_TLM, TLCD_ID_DEPLOY_TLM);
 }
 
