16b3e9811360fda7cb0f01b60a3f57935196dfaf
fukudakazuya <namiharrypotter@yahoo.co.jp>
2026-01-22 15:20:00 +0900

    Modify print.c

--- src/src_user/library/print.c
+++ src/src_user/library/print.c
@@ -2,15 +2,13 @@
  * @file
  * @brief  HW 依存 Print, つまり ログ情報のフォーマット出力（デバッグ出力）
  */
-// こいつもBootLoaderに配置するので，リプロ可能領域のものをincludeしないこと！！
 
-// printはHW依存性が強いため，ヘッダーはcoreにおき，オーバーライドする
-// ビルド対象にて選択する
 #include <src_core/library/print.h>
 
 #include <stdio.h>
 #include <stdarg.h>
 
+// #include "./print_arduino.h"
 #include "../settings/sils_define.h"
 #include "./RTT/SEGGER_RTT.h"
 
@@ -23,22 +21,26 @@ static char PRINT_buffer_[512];
 
 void Printf(const char* format, ...)
 {
+  // int threshold = 0;
   va_list argptr;
 
-  WDT_clear_wdt();         // 2019/03/10 追加
+  WDT_clear_wdt();
 
   va_start(argptr, format);
+  // threshold = vsprintf(PRINT_buffer_, format, argptr);
   SEGGER_RTT_vprintf(0, format, &argptr);
+
+  // printf_debug(PRINT_buffer_); // H/W dependent
   va_end(argptr);
 
-  WDT_clear_wdt();         // 2019/03/10 追加
+  WDT_clear_wdt();
 }
 
-#else
+#else // SILS用
 
 void Printf(const char* format, ...)
 {
-#ifdef SHOW_DEBUG_PRINT_ON_SILS
+  #ifdef SHOW_DEBUG_PRINT_ON_SILS
   va_list args;
   va_start(args, format);
   vsprintf(PRINT_buffer_, format, args);
