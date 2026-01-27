20827c9938ee726399f4899ed4cb398be0547e92
fukudakazuya <namiharrypotter@yahoo.co.jp>
2026-01-27 15:43:24 +0900

    Modify space_packet_typedef_params.h, packet_handler_params.h

--- src/src_user/settings/tlm_cmd/ccsds/space_packet_typedef_params.h
+++ src/src_user/settings/tlm_cmd/ccsds/space_packet_typedef_params.h
@@ -8,8 +8,10 @@
 #undef TSP_MAX_LEN
 #undef CSP_MAX_LEN
 
-// 223 だったが， 2022/02/01 に Space Packet 化したことで，ヘッダサイズが 13 byte 増えたので，とりあえず 240 に
-#define TSP_MAX_LEN  (240)
+// 240 だったが、STM32評価基盤に書き込む際にメモリ不足のエラーが出たので、
+// src/src_core/examples/subobc/src/src_user/settings/tlm_cmd/ccsds/space_packet_typedef_params.h 
+// と同じく432にした。
+#define TSP_MAX_LEN  (432)
 
 // double引数6個程度確保できるので64byteで十分
 #define CSP_MAX_LEN  (64) // 内15byteはヘッダなど
